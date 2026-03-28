f568410e64ddf4e882e2f799d36a33e7577a6471
IAmHaiel <mikhjnelo@gmail.com>
2026-03-28 10:31:17 +0800

    Removed gibberish

--- python.py
+++ python.py
@@ -1,13 +1,8 @@
 def add(a, b):
     return a + b + 15
 
-def subtract(a, b):rftebrtbrtbrtb
+def subtract(a, b):
     return a - b
 
 print("Add: ", add(5, 3))
 print("Subtract: ", subtract(5, 3))
-
-fwiuoegfhqeowrigwekrvbnlewkfowejlkqewvnlednvlsdfnvl kahvq
-dsgnmioweqgn
-gvoiweqrngv
-nvqioewrnv
