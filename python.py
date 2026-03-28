b8dba024e352be2f59abb923ba78c6f0350da2ba
IAmHaiel <mikhjnelo@gmail.com>
2026-03-28 11:11:41 +0800

    Changed

--- python.py
+++ python.py
@@ -5,6 +5,10 @@ def subtract(a, b):
     return a - b
 
 print("Add: ", add(5, 3))
+<<<<<<< HEAD
 print("Subtract: ", subtract(5, 3))
 
 52
+=======
+print("Subtract: ", subtract(5, 3))
+>>>>>>> parent of c19b9e4 (Contributed)
